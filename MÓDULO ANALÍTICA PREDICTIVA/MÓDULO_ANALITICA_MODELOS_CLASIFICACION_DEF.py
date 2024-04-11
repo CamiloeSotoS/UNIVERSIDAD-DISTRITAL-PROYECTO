e59518faa37bc1c07113dad2bff32fb7fb68ded5
CamiloeSotoS <155917455+CamiloeSotoS@users.noreply.github.com>
2024-04-11 11:53:46 -0500

    Ajuste

--- MÓDULO ANALÍTICA PREDICTIVA/MÓDULO_ANALITICA_MODELOS_CLASIFICACION_DEF.py
+++ MÓDULO ANALÍTICA PREDICTIVA/MÓDULO_ANALITICA_MODELOS_CLASIFICACION_DEF.py
@@ -2,6 +2,76 @@
 #---------------------------------------------------------------
 # LIBRERIAS
 #---------------------------------------------------------------
+
+
+
+import warnings
+warnings.filterwarnings('ignore')
+from numpy import set_printoptions
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+import numpy as np 
+import pandas as pd 
+pd.options.display.max_columns=None
+import seaborn as sns 
+from pandas import read_csv
+import io
+import base64
+import json
+from openpyxl import Workbook
+from openpyxl.drawing.image import Image as XLImage
+
+import os
+import os.path
+from unidecode import unidecode
+
+from urllib.error import HTTPError
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+from sklearn.model_selection import StratifiedKFold
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import PowerTransformer
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression 
+from catboost import CatBoostClassifier
+from xgboost import XGBClassifier
+from sklearn.svm import SVC
+from lightgbm import LGBMClassifier
+from mlens.ensemble import SuperLearner
+
+
+from sklearn.ensemble import AdaBoostClassifier 
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import StackingClassifier
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+from sklearn.metrics import normalized_mutual_info_score
+from sklearn.metrics import cohen_kappa_score
+
+
 import warnings
 warnings.filterwarnings('ignore')
 from numpy import set_printoptions
@@ -68,7 +138,7 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import StackingClassifier
-
+from sklearn.metrics import accuracy_score
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -78,6 +148,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 from sklearn.metrics import normalized_mutual_info_score
 from sklearn.metrics import cohen_kappa_score
+
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 # CARGUE DE DATOS
